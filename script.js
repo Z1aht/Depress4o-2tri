3979ff684ea5b6582156a99ea9d514e8eb6d737e
Z1aht <130074959+Z1aht@users.noreply.github.com>
2024-05-28 09:52:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+const caixaPrincipal = document.quarySelector(".caixa-principal");
+const caixaPerguntas = document.quarySelector(".caixa-perguntas");
+const caixaAlternativas = document.quarySelector(".caixa-alternativas");
+const caixaResultado = document.quarySelector(".caixa-resultado");
+const textoResultado = document.quarySelector(".texto-resultado");
 
+const perguntas = [
+  {
+    enunciado: "Pergunta 1",
+    alternativas;[
+      "alternativa 1",
+      "alternativa 2",
+  ]
+},
+  
